c908d9eb26738bfe5cf29762596cd4fe8799a041
lamrasli <badbadr23a@gmail.com>
2025-03-19 01:34:38 +0000

    Suppression de l'import inutilisé Link

--- src/components/Pages/EquipmentDocumentation/EquipmentDocumentation.jsx
+++ src/components/Pages/EquipmentDocumentation/EquipmentDocumentation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom"; // Ajout de useParams et Link
+import { useParams,} from "react-router-dom"; // Ajout de useParams et Link
 import {
   FaMicrochip,
   FaDesktop,
